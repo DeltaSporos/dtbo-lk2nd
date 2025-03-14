14afde460e6a973468dd1b6275dfdb47642e9b15
DeltaSporos <60284244+DeltaSporos@users.noreply.github.com>
2025-03-14 06:58:15 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ dtc -O dtb -o sdm632-xiaomi-onclite.dtbo -b 0 -@ sdm632-xiaomi-onclite.dts
 mkdtboimg cfg_create dtbo-xiaomi-onclite.img dtboimg-xiaomi-onclite.cfg
 dtc -O dtb -o sdm439-xiaomi-pine.dtbo -b 0 -@ sdm439-xiaomi-pine.dts
 mkdtboimg cfg_create dtbo-xiaomi-pine.img dtboimg-xiaomi-pine.cfg
+
+# CAT S22 Flip
+mkdtboimg cfg_create dtbo-cat-s22flip.img dtboimg-cat-s22flip.cfg
